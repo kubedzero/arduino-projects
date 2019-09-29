8acb1cf10f0fa1a85f0bf26356967490955d9ea9
KZ <kubedzero@gmail.com>
2019-09-28 18:04:14 -0700

    Cleanup of code, add methodized sensor retrieval and printout

--- sensorWebServer/sensorWebServer.ino
+++ sensorWebServer/sensorWebServer.ino
@@ -4,17 +4,18 @@
 #include <ESP8266WebServer.h> // library for HTTP Server
 #include <ESP8266HTTPUpdateServer.h> // library for update code
 
-#include "myCredentials.h" //used to store WiFi and update credentials
+#include "myCredentials.h" // used to store WiFi and update credentials
 
 // libraries for sensor reading
 #include <Wire.h> // for BMP280 or BME280 via I2C
 #include <Adafruit_Sensor.h> // Adafruit unified sensor library
 #include <Adafruit_BMP280.h> // Adafruit extension library for BMP280
 #include <Adafruit_BME280.h> // Adafruit extension library for BME280
-#include <DHT.h> //Adafruit extension library DHT22
+#include <DHT.h> // Adafruit extension library for DHT22
 
 // other libraries
-#include <string.h> //string comparison
+#include <string.h> // string comparison
+#include <float.h> // Float methods and constants
 
 
 // OTA update constants
@@ -23,9 +24,8 @@ const char* update_username = WEB_UPDATE_USER; // from creds file
 const char* update_password = WEB_UPDATE_PASS; // from creds file
 const char* ssid = WIFI_SSID; // from creds file
 const char* password = WIFI_PASSWD; // from creds file
+#define SERIAL_BAUD 115200 // baud rate for Serial debugging
 
-
-#define SERIAL_BAUD 115200 //baud rate for Serial debugging
 
 // Sensor inits and constants
 String boschType = "uninitialized";
@@ -38,6 +38,22 @@ DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor.
 
 ESP8266WebServer httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
+
+
+// Sensor data global vars
+// DHT Data
+float dhtHumidityPercent = 0;
+float dhtTemperatureC = 0;
+float dhtTemperatureF = 0;
+
+// Bosch Data
+float boschHumidityPercent = 0;
+float boschTemperatureC = 0;
+float boschTemperatureF = 0;
+float boschPressurePa = 0;
+float boschPressureInHg = 0;
+
+
 
 // method to connect to and initialize whichever Bosch sensor is connected
 void setupBosch() {
@@ -52,8 +68,7 @@ void setupBosch() {
   Serial.println("ID of 0x60 represents a BME 280");
   Serial.println("ID of 0x61 represents a BME 680");
   if (!boschStatus) {
-    Serial.println("Could not find a valid BME280, check wiring, address, sensor ID!");
-    delay(2000);
+    Serial.println("No BME280 found, moving to check next Bosch sensor");
   } else {
     Serial.println("\nBME280 found!\n");
     boschType = "BME280";
@@ -68,9 +83,7 @@ void setupBosch() {
 
   // Try BMP280 setup
   boschStatus = bmp280.begin(0x76, 0x58); //BMP280 has I2C address 0x76 and chipID of 0x58
-  if (!boschStatus) {
-    Serial.println("Could not find a valid BMP280, check wiring, address, sensor ID!");
-  } else {
+  if (boschStatus) {
     Serial.println("\nBMP280 found!\n");
     boschType = "BMP280";
     /* Default settings from datasheet. */
@@ -79,97 +92,102 @@ void setupBosch() {
                        Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                        Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                        Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-    return;
+  }
+
+}
+
+// Logic to handle updating the global sensor data vars from present sensors
+void updateSensorData() {
+
+  // Get DHT Data
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (it is a very slow sensor)
+  float currentDhtHumidityPercent = dht.readHumidity();
+  float currentDhtTemperatureC = dht.readTemperature();
+  float currentDhtTemperatureF = dht.readTemperature(true);
+  // Check if any reads failed and print error with values
+  if (isnan(currentDhtHumidityPercent)
+      || isnan(currentDhtTemperatureC)
+      || isnan(currentDhtTemperatureF)) {
+    Serial.print(F("Failed to read from DHT sensor! Values were"));
+    Serial.print(F("Humidity: "));
+    Serial.print(currentDhtHumidityPercent);
+    Serial.print(F("%  Temperature: "));
+    Serial.print(currentDhtTemperatureC);
+    Serial.print(F("°C "));
+    Serial.print(currentDhtTemperatureF);
+    Serial.println(F("°F "));
+  }
+  // update global values
+  dhtHumidityPercent = currentDhtHumidityPercent;
+  dhtTemperatureC = currentDhtTemperatureC;
+  dhtTemperatureF = currentDhtTemperatureF;
+
+  // Get Bosch Data
+  if (boschType.equals("BMP280")) {
+    boschHumidityPercent = 1.0 / 0.0; // intentionally set this to NaN
+    boschTemperatureC = bmp280.readTemperature();
+    boschTemperatureF = boschTemperatureC * (9.0F / 5.0F) + 32.0F;
+    boschPressurePa = bmp280.readPressure();
+    boschPressureInHg = boschPressurePa / 3386.38866F;
+
+  } else if (boschType.equals("BME280")) {
+    boschHumidityPercent = bme280.readHumidity();
+    boschTemperatureC = bme280.readTemperature();
+    boschTemperatureF = boschTemperatureC * (9 / 5) + 32;
+    boschPressurePa = bme280.readPressure();
+    boschPressureInHg = boschPressurePa / 3386.38866;
+  } else if (boschType.equals("uninitialized")) {
+    Serial.println("Could not find a valid BMx280, check wiring, address, sensor ID!");
+
   }
 
 }
 
 void setup(void) {
-  pinMode(LED_BUILTIN, OUTPUT); // Blue(GeekCreit) or Red(NodeMCU 0.9) LED initialized LOW (LED ON)
-
   Serial.begin(SERIAL_BAUD);
   Serial.println("\nBooting Sketch...\n");
 
-
+  pinMode(LED_BUILTIN, OUTPUT); // Blue(GeekCreit) or Red(NodeMCU 0.9) LED initialized LOW (LED ON)
   // Connect to WiFi network
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.mode(WIFI_AP_STA);
   WiFi.begin(ssid, password);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
   Serial.print("\nConnected, IP address is ");
   Serial.println(WiFi.localIP());
+
+  digitalWrite(LED_BUILTIN, HIGH); // set Blue(GeekCreit) or Red(NodeMCU 0.9) LED to off
+
+  setupBosch(); // run the setup method to initialize one Bosch sensor
+  dht.begin(); // initialize the DHT sensor
 
   httpUpdater.setup(&httpServer, update_path, update_username, update_password); // OTA server setup
 
   httpServer.begin();
-
-  digitalWrite(LED_BUILTIN, HIGH); //set Blue(GeekCreit) or Red(NodeMCU 0.9) LED to off
-
-  setupBosch();
-  dht.begin();
 }
 
 void loop(void) {
   httpServer.handleClient();
-  if (boschType.equals("BMP280")) {
-
-    Serial.print(F("Temperature = "));
-    Serial.print(bmp280.readTemperature());
-    Serial.println(" *C");
-
-    Serial.print(F("Pressure = "));
-    Serial.print(bmp280.readPressure());
-    Serial.println(" Pa");
-
-    Serial.println();
-    delay(2000);
-  } else if (boschType.equals("BME280")) {
-    printValues();
-  } else {}
-
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
-
-  }
-
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
-  Serial.print(F("%  Temperature: "));
-  Serial.print(t);
-  Serial.print(F("°C "));
-  Serial.println(f);
-  Serial.print(F("°F "));
+  updateSensorData();
+  printValues();
+  delay(2000);
 }
 
 void printValues() {
-  Serial.print("Temperature = ");
-  Serial.print(bme280.readTemperature());
-  Serial.println(" *C");
-
-  Serial.print("Pressure = ");
-
-  Serial.print(bme280.readPressure() / 100.0F);
-  Serial.println(" hPa");
-
-  Serial.print("Humidity = ");
-  Serial.print(bme280.readHumidity());
-  Serial.println(" %");
-
+  Serial.println("Values:");
+  Serial.println("dhtHumidityPercent " + String(dhtHumidityPercent, 2));
+  Serial.println("dhtTemperatureC " + String(dhtTemperatureC, 2));
+  Serial.println("dhtTemperatureF " + String(dhtTemperatureF, 2));
+  Serial.println("boschHumidityPercent " + String(boschHumidityPercent, 2));
+  Serial.println("boschTemperatureC " + String(boschTemperatureC, 2));
+  Serial.println("boschTemperatureF " + String(boschTemperatureF, 2));
+  Serial.println("boschPressurePa " + String(boschPressurePa, 2));
+  Serial.println("boschPressureInHg " + String(boschPressureInHg, 2));
   Serial.println();
-  delay(2000);
+
 }
